40689f89fc7abd4135ea4beb73a6aa5a8c4b031b
mahdiharish <mahdiharish12@gmail.com>
2023-10-06 10:29:48 +0800

    fix pathing on index and add more data

---
+++ src/data/education.ts
@@ -0,0 +1,21 @@
+type Education = {
+    title: string;
+    institution: string;
+    educationTime: string;
+    description: string;
+}
+
+const education: Education[] = [
+    {
+        title: "Full Stack Software Engineering",
+        institution: "RevoU",
+        educationTime: "January 2023 - August 2023",
+        description: "Finished 27 weeks of intensive bootcamp related to web and software development, with tech stacks such as JavaScript, TypeScript, React.js, Next.js, NestJS, Docker, etc."
+    },
+    {
+        title: "Bachelor of Food Technology",
+        institution: "Brawijaya University",
+        educationTime: "August 2014 - August 2018",
+        description: "Graduated with GPA of 3.18."
+    }
+]
---
+++ src/data/experience.ts
@@ -0,0 +1,29 @@
+type WorkExperience = {
+    title: string;
+    company: string;
+    workTime: string;
+    description: string;
+}
+
+const workExperience: WorkExperience[] = [
+    {
+        title: "Independent Contractor",
+        company: "TELUS International",
+        workTime: "July 2022 - August 2023",
+        description: "Examining search engine content to fit users' queries and intents in Microsoft Bing, as well as its appropriateness. Also researching contents befitting users' queries and intents and keywords related to them."
+    },
+    {
+        title: "Client Relations Specialist",
+        company: "Maxim Order Service",
+        workTime: "September 2020 - June 2022",
+        description: "Increasing brand visibilities throughout the city in order to inrease total orders, engaging with clients, drivers, and partners including processing queries, complaints, and applications as well as necessary follow ups. I also become Branding Manager and responsible for increasing branding awareness through car branding in city, as well as managing other cities too."
+    },
+    {
+        title: "Independent Contractor",
+        company: "Appen Ltd",
+        workTime: "June 2020 - May 2021",
+        description: "Checking Instagram ads whether they are related with users' interest, and also its appropriateness and usability."
+    }
+]
+
+export default workExperience;
