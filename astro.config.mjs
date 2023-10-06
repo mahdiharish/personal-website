099f5fac3edafb096f9e639b02df43cdbb04964d
mahdiharish <mahdiharish12@gmail.com>
2023-10-06 23:00:16 +0800

    dependency: fix dependency of sharp

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
-import { defineConfig, passthroughImageService } from "astro/config";
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  image: {
-    service: passthroughImageService(),
-  },
 });
