4470325f342202f01ea4acce32db712932cc060b
mahdiharish <mahdiharish12@gmail.com>
2023-10-07 07:31:37 +0800

    style: add styling on background

---
+++ src/data/theme.ts
@@ -0,0 +1,23 @@
+import type { TailwindColor } from "@/utils/types/tailwind";
+
+type Theme = {
+  colors: {
+    primary: TailwindColor;
+    blur: {
+      top: TailwindColor;
+      bottom: TailwindColor;
+    };
+  };
+};
+
+const theme: Theme = {
+  colors: {
+    primary: "blue",
+    blur: {
+      top: "teal",
+      bottom: "indigo",
+    },
+  },
+};
+
+export default theme;
--- src/utils/types/tailwind.ts
+++ src/utils/types/tailwind.ts
@@ -10,3 +10,8 @@ export type TailwindColor =
   | "teal"
   | "cyan"
   | "blue"
+  | "indigo"
+  | "purple"
+  | "fushia"
+  | "pink"
+  | "rose";
